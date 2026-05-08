2580bc24bd06f9bd9bb83eda8f8fbfd28f678e26
Andrii Gulevatenko <otobrazheniye@gmail.com>
2026-05-08 13:24:54 +0200

    classes Topic, Article temp complited

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 class Topic(models.Model):
     name = models.CharField(max_length=100)
+
     created_at = models.DateTimeField(auto_now_add=True)
     subscribers = models.ManyToManyField(User,related_name="subscribed_topics", blank = True)  
 
@@ -11,9 +12,12 @@ class Topic(models.Model):
     
 class Article(models.Model):
     author = models.ForeignKey(User,related_name="articles",on_delete=models.CASCADE)
+    topics = models.ManyToManyField(Topic,related_name="articles",blank = False)
     title = models.CharField(max_length=200)
+
     content = models.TextField(max_length=1000)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.title}"
+    
