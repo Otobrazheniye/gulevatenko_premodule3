a1838459cdb8914ad436a1dcbdef707e8e56de12
Andrii Gulevatenko <otobrazheniye@gmail.com>
2026-05-08 13:42:00 +0200

    model comment temp complited

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Topic(models.Model):
     name = models.CharField(max_length=100)
 
@@ -9,15 +10,27 @@ class Topic(models.Model):
 
     def __str__(self):
         return f"{self.name}"
-    
+
+
 class Article(models.Model):
     author = models.ForeignKey(User,related_name="articles",on_delete=models.CASCADE)
     topics = models.ManyToManyField(Topic,related_name="articles",blank = False)
+
     title = models.CharField(max_length=200)
 
-    content = models.TextField(max_length=1000)
+    content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.title}"
     
+
+class Comment(models.Model):
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    author = models.ForeignKey(User,related_name="comments",on_delete=models.CASCADE)
+    article = models.ForeignKey(Article,related_name="comments",on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Comment by {self.author} \n\t{self.text}"
