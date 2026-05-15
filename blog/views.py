22d7623123939a8816ac2cd118889ed944a6be94
Andrii Gulevatenko <otobrazheniye@gmail.com>
2026-05-15 20:48:29 +0200

    complite test profile, time logic

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Article, Topic, Comment
 from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 
 
 def article_list(request):
@@ -14,8 +15,22 @@ def article_list(request):
     return render(request, 'blog/article_list.html', context)
 
 def my_feed(request):
-    return render(request,'blog/my_feed.html')
+    if request.user.is_authenticated:
+        user_topics = request.user.subscribed_topics.all()
 
+        articles = Article.objects.filter(
+            topics__in=user_topics
+        ).distinct().order_by('-created_at')
+    else:
+        user_topics = []
+        articles = []
+
+    context = {
+        'user_topics': user_topics,
+        'articles': articles,
+    }
+
+    return render(request, 'blog/my_feed.html', context)
 
 def article_detail(request, article_id):
     article = get_object_or_404(Article, id=article_id)
@@ -74,7 +89,22 @@ def topic_unsubscribe(request, topic_id):
 
 
 def profile(request):
-    return render(request,'blog/profile.html')
+    if request.user.is_authenticated:
+        user_topics = request.user.subscribed_topics.all()
+        user_articles = request.user.articles.all().order_by('-created_at')
+        user_comments = request.user.comments.all().order_by('-created_at')
+    else:
+        user_topics = []
+        user_articles = []
+        user_comments = []
+
+    context = {
+        'user_topics': user_topics,
+        'user_articles': user_articles,
+        'user_comments': user_comments,
+    }
+
+    return render(request, 'blog/profile.html', context)
 
 
 def register(request):
@@ -93,6 +123,25 @@ def logout_view(request):
     return HttpResponse('Логаут')
 
 
+
+
 def article_archive(request, year, month):
-    return render(request, 'blog/article_archive.html')
+    if year < 1000 or year > 9999:
+        raise Http404('Invalid year')
+
+    if month < 1 or month > 12:
+        raise Http404('Invalid month')
+
+    articles = Article.objects.filter(
+        created_at__year=year,
+        created_at__month=month,
+    ).order_by('-created_at')
+
+    context = {
+        'year': year,
+        'month': month,
+        'articles': articles,
+    }
+
+    return render(request, 'blog/article_archive.html', context)
 
